1cbc4a3a3f42b3d990c74f9c0d142d0264344ebb
Oleg Karasev <o.karasev@tinkoff.ru>
2021-08-15 10:04:23 +0300

    pep8 format

--- git_web_link.py
+++ git_web_link.py
@@ -12,11 +12,11 @@ import webbrowser
 import argparse
 
 # Configurable regex patterns for subversion systems. First matched pattern will be used.
-urlPatterns=[
-  {
-    # like git@github.com:okar1/git_web_link.git
-    "name": "github ssh",
-    "findWhat": r"""^
+urlPatterns = [
+    {
+        # like git@github.com:okar1/git_web_link.git
+        "name": "github ssh",
+        "findWhat": r"""^
         # ssh
         (ssh://)?
         # git@, optional
@@ -31,15 +31,15 @@ urlPatterns=[
         (?P<group_and_repo>(\w|_|-|\.|/)+)
         .git
         $""",
-    "replaceWith": r"https://\g<host>/\g<login>/\g<group_and_repo>/{linkType}/{branchName}/{path}{lineNumberSection}",
-    "lineNumberSection": "#L{linenumber}",
-    "linkTypeFile": "blob",
-    "linkTypeDir": "tree"
-  },
-  {
-    # like https://github.com/okar1/git_web_link.git
-    "name": "github https",
-    "findWhat": r"""^
+        "replaceWith": r"https://\g<host>/\g<login>/\g<group_and_repo>/{linkType}/{branchName}/{path}{lineNumberSection}",
+        "lineNumberSection": "#L{linenumber}",
+        "linkTypeFile": "blob",
+        "linkTypeDir": "tree"
+    },
+    {
+        # like https://github.com/okar1/git_web_link.git
+        "name": "github https",
+        "findWhat": r"""^
         # https
         (https://)?
         # github.com
@@ -52,14 +52,14 @@ urlPatterns=[
         (?P<group_and_repo>(\w|_|-|\.|/)+)
         .git
         $""",
-    "replaceWith": r"https://\g<host>/\g<login>/\g<group_and_repo>/{linkType}/{branchName}/{path}{lineNumberSection}",
-    "lineNumberSection": "#L{linenumber}",
-    "linkTypeFile": "blob",
-    "linkTypeDir": "tree"
-  },
-  {
-    "name": "stash ssh and https",
-    "findWhat": r"""^
+        "replaceWith": r"https://\g<host>/\g<login>/\g<group_and_repo>/{linkType}/{branchName}/{path}{lineNumberSection}",
+        "lineNumberSection": "#L{linenumber}",
+        "linkTypeFile": "blob",
+        "linkTypeDir": "tree"
+    },
+    {
+        "name": "stash ssh and https",
+        "findWhat": r"""^
         # ssh
         (?P<proto>ssh|http|https)
         ://
@@ -77,14 +77,14 @@ urlPatterns=[
         (?P<group_and_repo>(\w|_|-|\.|/)+)
         .git
         $""",
-    "replaceWith": r"https://\g<host>/projects/\g<project>/repos/\g<group_and_repo>/browse/{path}?at=refs%2Fheads%2F{branchName}{lineNumberSection}",
-    "lineNumberSection": "#{linenumber}",
-    "linkTypeFile": None,
-    "linkTypeDir": None
-  },
-  {
-    "name": "gitlab ssh and https",
-    "findWhat": r"""^
+        "replaceWith": r"https://\g<host>/projects/\g<project>/repos/\g<group_and_repo>/browse/{path}?at=refs%2Fheads%2F{branchName}{lineNumberSection}",
+        "lineNumberSection": "#{linenumber}",
+        "linkTypeFile": None,
+        "linkTypeDir": None
+    },
+    {
+        "name": "gitlab ssh and https",
+        "findWhat": r"""^
         # ssh
         (?P<proto>ssh|http|https)
         ://
@@ -102,17 +102,18 @@ urlPatterns=[
         (?P<group_and_repo>(\w|_|-|\.|/)+)
         .git
         $""",
-    "replaceWith": r"https://\g<host>/\g<project>/\g<group_and_repo>/-/{linkType}/{branchName}/{path}{lineNumberSection}",
-    "lineNumberSection": "#L{linenumber}",
-    "linkTypeFile": "blob",
-    "linkTypeDir": "tree"
-  },
+        "replaceWith": r"https://\g<host>/\g<project>/\g<group_and_repo>/-/{linkType}/{branchName}/{path}{lineNumberSection}",
+        "lineNumberSection": "#L{linenumber}",
+        "linkTypeFile": "blob",
+        "linkTypeDir": "tree"
+    },
 ]
 
 
-parser = argparse.ArgumentParser(description = 'Script opens gitlab/github/stash/etc web page for current local file/dir within your repo.')
-parser.add_argument('path', type = str, nargs = '?', default = os.getcwd(), help = 'path to local file or dir within repo')
-parser.add_argument('lineToHighlight', type = int, nargs = '?', default = 0, help = 'line number to highlight in git site')
+parser = argparse.ArgumentParser(
+    description='Script opens gitlab/github/stash/etc web page for current local file/dir within your repo.')
+parser.add_argument('path', type=str, nargs='?', default=os.getcwd(), help='path to local file or dir within repo')
+parser.add_argument('lineToHighlight', type=int, nargs='?', default=0, help='line number to highlight in git site')
 
 args = parser.parse_args()
 
@@ -120,10 +121,11 @@ absPath = os.path.abspath(args.path)
 lineToHighlight = args.lineToHighlight if os.path.isfile(absPath) else 0
 
 try:
-  repo=Repo(absPath, search_parent_directories=True)
+  repo = Repo(absPath, search_parent_directories=True)
 except (InvalidGitRepositoryError, NoSuchPathError):
   parser.print_help()
-  print(f"ERROR specified path {absPath} is not a part of valid git repository")
+  print(
+      f"ERROR specified path {absPath} is not a part of valid git repository")
   sys.exit(1)
 
 if not repo.remotes:
@@ -140,29 +142,34 @@ remoteUrl = remoteUrls[0]
 
 if not absPath.startswith(repo.working_dir):
   parser.print_help()
-  print(f'ERROR something is wrong. Path to process "{absPath}" is not starting from git working dir "{repo.working_dir}"')
+  print(
+      f'ERROR something is wrong. Path to process "{absPath}" is not starting from git working dir "{repo.working_dir}"')
   sys.exit(1)
 
-relativePath='' if absPath==repo.working_dir else absPath[len(repo.working_dir)+1:]
+relativePath = '' if absPath == repo.working_dir else absPath[len(
+    repo.working_dir) + 1:]
 banchName = repo.active_branch.name
 
 
-UrlPattern=namedtuple('UrlPattern','name findWhat replaceWith linkTypeFile linkTypeDir lineNumberSection')
+UrlPattern = namedtuple(
+    'UrlPattern', 'name findWhat replaceWith linkTypeFile linkTypeDir lineNumberSection')
 
 for patternName, findWhat, replaceWith, linkTypeFile, linkTypeDir, lineNumberSection in map(lambda a: UrlPattern(**a), urlPatterns):
   webUrl = re.sub(findWhat, replaceWith, remoteUrl, flags=re.VERBOSE)
   if webUrl != remoteUrl:
     # replacement found
     webUrl = webUrl.format(
-      branchName = banchName,
-      path = relativePath,
-      lineNumberSection = lineNumberSection.format(linenumber = lineToHighlight) if lineToHighlight else '',
-      linkType = linkTypeFile if os.path.isfile(absPath) else linkTypeDir
+        branchName=banchName,
+        path=relativePath,
+        lineNumberSection=lineNumberSection.format(
+            linenumber=lineToHighlight) if lineToHighlight else '',
+        linkType=linkTypeFile if os.path.isfile(absPath) else linkTypeDir
     )
     print("found remote url", remoteUrl)
     print(f'converted to web url using pattern "{patternName}":', webUrl)
     webbrowser.open(webUrl)
     break
 else:
-  print(f'ERROR could not find replacement for origin "{remoteUrl}", check regex of urlPatterns')
+  print(
+      f'ERROR could not find replacement for origin "{remoteUrl}", check regex of urlPatterns')
   sys.exit(1)
